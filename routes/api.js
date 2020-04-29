8ad6abaacb71c3ac92f400b8c90c7f23165d6aa0
aaron5670 <a.vdberg98@gmail.com>
2020-04-29 22:58:34 +0200

    :zap: Improved API url

--- routes/api.js
+++ routes/api.js
@@ -2,6 +2,6 @@ const express = require('express');
 const api = express.Router();
 const v1Route = require('./v1/init');
 
-api.use('/v1/', v1Route);
+api.use('/v1', v1Route);
 
 module.exports = api;
